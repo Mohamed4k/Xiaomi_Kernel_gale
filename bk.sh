31a242d4346498c8c5aa53faeb8a64656c170157
Mohamed4k <aaamohammed603@gmail.com>
2025-01-03 21:35:32 +0200

    Update bk.sh

--- bk.sh
+++ bk.sh
@@ -9,7 +9,7 @@ cd Xiaomi_Kernel_gale
 git clone https://gitlab.com/LeCmnGend/clang.git -b clang-18 --depth=1
 
 
-TC_DIR="$(pwd)/clang-18"
+TC_DIR="$(pwd)/clang"
 
 export PATH="$TC_DIR/bin:$PATH"
 
