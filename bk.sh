833eb4916bd713b9bbc9e900ceac9f8cbe448f16
Mohamed4k <aaamohammed603@gmail.com>
2025-01-03 21:12:12 +0200

    Add files via upload

---
+++ bk.sh
@@ -0,0 +1,18 @@
+sudo apt-get install clang-format clang-tidy clang-tools clang clangd libc++-dev libc++1 libc++abi-dev libc++abi1 libclang-dev libclang1 liblldb-dev libllvm-ocaml-dev libomp-dev libomp5 lld lldb llvm-dev llvm-runtime llvm python3-clang
+
+sudo apt-get install gcc-aarch64-linux-gnu
+
+git clone --depth=1 https://github.com/Mohamed4k/Xiaomi_Kernel_gale.git -b gale-s-oss
+
+cd Xiaomi_Kernel_gale
+
+git clone https://gitlab.com/LeCmnGend/clang.git -b clang-18 --depth=1
+
+TC_DIR="clang"
+
+export PATH="$TC_DIR/bin:$PATH"
+
+mkdir -p out
+make O=out ARCH=arm64 gale_defconfig
+
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- 2>&1 | tee log.txt
